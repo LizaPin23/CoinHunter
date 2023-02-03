5aad67106d1b4454a1ed6ce286b3edc6066300f8
LizaPin23 <111238411+LizaPin23@users.noreply.github.com>
2023-02-03 18:57:22 +0300

    something

---
+++ Assets/CoinHunter/Shared/Sources/CameraFollower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField] private Transform _transform;
+    private const float _z = -10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(_transform.position.x, _transform.position.y, _z);
+    }
+}
--- Assets/CoinHunter/Shared/Sources/Home.cs
+++ Assets/CoinHunter/Shared/Sources/Home.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class Home : MonoBehaviour
 {
-     
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private float _force = 3f;
+
+    private Vector2 direction => Vector2.right;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            OnRButtonPressed();
+        }
+    }   
+    
+    public void OnRButtonPressed()
+    {
+        _rigidbody.AddForce(direction * _force, ForceMode2D.Impulse);
+        Debug.Log("вызывается");
+    }
 }
