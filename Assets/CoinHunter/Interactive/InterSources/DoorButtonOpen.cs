bdc8c3152862b21702bfb16bc2fb0a0c221afb9b
LizaPin23 <111238411+LizaPin23@users.noreply.github.com>
2023-02-19 18:37:47 +0300

    qwertyuiokjhgf

--- Assets/CoinHunter/Interactive/InterSources/DoorButtonOpen.cs
+++ Assets/CoinHunter/Interactive/InterSources/DoorButtonOpen.cs
@@ -2,7 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorButtonOpen : MonoBehaviour
+namespace CoinHunter.Levels.Interactive
 {
-    [SerializeField] private 
+    public class DoorButtonOpen : MonoBehaviour
+    {
+        [SerializeField] private DoorOpen _doorOpen;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.TryGetComponent<ButtonBlock>(out ButtonBlock buttonBlock))
+            {
+               //_doorOpen.
+            }
+        }
+    }
+
 }
--- Assets/CoinHunter/Interactive/InterSources/DoorOpen.cs
+++ Assets/CoinHunter/Interactive/InterSources/DoorOpen.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorOpen : MonoBehaviour
+namespace CoinHunter.Levels.Interactive
 {
-    // Start is called before the first frame update
-    void Start()
+    public class DoorOpen : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Animator _animator;
+        [SerializeField] private string _ = "Moving";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        private void Open()
+        {
+
+        }
     }
 }
+
