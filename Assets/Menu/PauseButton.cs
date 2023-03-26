ae93b4e2671498964fb3602e6426e24ffb135e63
LizaPin23 <111238411+LizaPin23@users.noreply.github.com>
2023-03-26 12:00:18 +0300

    sth

---
+++ Assets/Menu/PauseButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    public event Action Pressed;
+
+    public void SetVisible(bool sth)
+    {
+        gameObject.SetActive(sth);
+    }
+
+    public void OnPress()
+    {
+        Pressed?.Invoke();
+    }
+}
---
+++ Assets/Menu/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public event Action QuitButtonPressed;
+    public event Action ContinueButtonPressed;
+    public event Action PauseButtonPressed;
+
+    private void Awake()
+    {
+        SetVisible(false);
+    }
+
+    public void SetVisible(bool sth)
+    {
+        gameObject.SetActive(sth);
+    }
+
+    public void OnQuitButtonPressed()
+    {
+        QuitButtonPressed?.Invoke();
+    }
+
+    public void OnContinueButtonPressed()
+    {
+        ContinueButtonPressed?.Invoke();
+    }
+}
