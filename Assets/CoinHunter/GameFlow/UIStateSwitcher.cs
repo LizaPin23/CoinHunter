e4ec1badf6a834f802a60809724d94178d4c40cf
LizaPin23 <111238411+LizaPin23@users.noreply.github.com>
2023-03-31 18:00:29 +0300

    zlwnt

--- Assets/CoinHunter/GameFlow/UIStateSwitcher.cs
+++ Assets/CoinHunter/GameFlow/UIStateSwitcher.cs
@@ -4,9 +4,18 @@ namespace CoinHunter.GameFlow
 {
     public class UIStateSwitcher : MonoBehaviour, IGameStateListener
     {
+        [SerializeField] private int _windows;
+        //временно инт
+        // в зависимости от значения виндоус включает то или иное окошко
+        // попробовавть создать лист с окошками чтобы добавлять окошки, но это не точно
         public void OnGameStateChanged(GameState value)
         {
             throw new System.NotImplementedException();
+        }
+
+        public void Perecluchenie()
+        {
+
         }
     }
 }
