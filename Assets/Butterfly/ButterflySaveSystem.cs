6ce0e877d8bec7236055b4c9a6f12c1b3a2818f7
LizaPin23 <111238411+LizaPin23@users.noreply.github.com>
2023-06-10 12:59:00 +0300

    TryToDoHWButILostMyMouse

---
+++ Assets/Butterfly/ButterflySaveSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflySaveSystem : MonoBehaviour
+{
+    private Butterflies _butter;
+    public int ButterfliesAmount => _butter.ButterfliesAmount;
+    public string ButterflyColor => _butter.ButterflyColor;
+    public int YellowButterflies => _butter.YellowButterflies;
+
+    private const string prefsKey = "Save";
+
+
+    public void Save()
+    {
+        string jsonData = JsonUtility.ToJson(_butter);
+        PlayerPrefs.SetString(prefsKey, jsonData);
+    }
+
+    public void Load()
+    {
+        
+    }
+}
